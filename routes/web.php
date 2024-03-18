471ba6cabd721921ab513a5b98e810af572ebcf3
john <john@softsmart.co.za>
2024-03-18 11:30:07 +0200

    Initial commit

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get("/name", function () {
+    return fake()->name();
+});
+
 Route::get('/', function () {
     return view('welcome');
 });
